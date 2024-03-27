5e47d5480620229fa5f598685d5ec192bfb120b3
Madu-f0r0 <duherbert@gmail.com>
2024-03-27 15:58:30 +0000

    Completed the counter function to tount call requency of methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,6 +27,7 @@ class Cache:
         self._redis = redis.Redis(host="localhost", port=6379, db=0)
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Inputs data in redis using a random key and returns the key"""
         key = str(uuid.uuid4())
